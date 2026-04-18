2e128cd3248c97319e5a887b588fa53cf9ab8b49
Nathan K <nathankidd@hey.com>
2026-04-18 04:15:59 +0000

    restructure

--- lib/string_builder.rb
+++ lib/string_builder.rb
@@ -1,5 +1,117 @@
 # frozen_string_literal: true
 
 require_relative "string_builder/version"
-require_relative "string_builder/core"
-require_relative "string_builder/monkey_patch"
+
+class StringBuilder
+  include Enumerable
+
+  attr_reader :buffer
+  attr_accessor :parser
+
+  DEFAULT_PARSER = -> (buffer) {
+    buffer.map { |entry|
+      case entry
+      when :slash then "/"
+      when :dash then "-"
+      else
+        name, args = entry
+        args.empty? ? name : "#{name}(#{args.map(&:inspect).join(', ')})"
+      end
+    }.join(" ")
+  }
+
+  def initialize(&custom_parser)
+    @buffer = []
+    @parser = custom_parser || DEFAULT_PARSER
+    @operator_pos = nil
+  end
+
+  def to_s
+    @parser.call(@buffer)
+  end
+
+  def wrap(&block)
+    instance_eval(&block)
+    self
+  end
+
+  def each
+    yield @buffer
+  end
+
+  def respond_to_missing?(_name, _include_private = false)
+    true
+  end
+
+  def method_missing(name, *args, &_block)
+    tap do
+      @buffer << [name.to_s, args]
+    end
+  end
+
+  def call(token)
+    tap do
+      @buffer << [token.to_s, []]
+    end
+  end
+
+  def /(_other)
+    tap do
+      insert_pos = @operator_pos || (@buffer.length - 1)
+      @buffer.insert(insert_pos, :slash)
+      @operator_pos = @buffer.length
+    end
+  end
+
+  def -(other)
+    tap do
+      case other
+      when MethodCallToken
+        @buffer << [other.base.to_s, []]
+        insert_pos = @operator_pos || (@buffer.length - 1)
+        @buffer.insert(insert_pos, :dash)
+        @buffer << [other.name.to_s, other.args]
+        @operator_pos = @buffer.length
+      else
+        unless other.is_a?(StringBuilder)
+          @buffer << [other.to_s, []]
+        end
+        insert_pos = @operator_pos || (@buffer.length - 1)
+        @buffer.insert(insert_pos, :dash)
+        @operator_pos = @buffer.length
+      end
+    end
+  end
+end
+
+class ::Integer
+  def method_missing(name, *args, &_block)
+    return super unless name.to_s.match?(/\A[a-z_][a-z0-9_]*\z/)
+
+    MethodCallToken.new(self, name, args)
+  end
+
+  def respond_to_missing?(name, include_private = false)
+    name.to_s.match?(/\A[a-z_][a-z0-9_]*\z/) || super
+  end
+
+  def to_str
+    to_s
+  end
+end
+
+class MethodCallToken
+  attr_reader :base
+  attr_reader :name
+  attr_reader :args
+
+  def initialize(base, name, args)
+    @base = base
+    @name = name
+    @args = args
+  end
+
+  def to_s
+    "#{base}.#{name}"
+  end
+end
--- test/string_builder_test.rb
+++ test/string_builder_test.rb
@@ -11,24 +11,32 @@ class StringBuilderTest < Minitest::Test
     assert_equal expected, result.buffer
   end
 
+  def assert_string(result, expected)
+    assert_equal expected, result.to_s
+  end
+
   def test_get
     result = sb { get }
     assert_buffer(result, [["get", []]])
+    assert_string(result, "get")
   end
 
   def test_get_deployment
     result = sb { get.deployment }
     assert_buffer(result, [["get", []], ["deployment", []]])
+    assert_string(result, "get deployment")
   end
 
   def test_get_deployment_slash_v1
     result = sb { get.deployment/v1 }
     assert_buffer(result, [["get", []], ["deployment", []], :slash, ["v1", []]])
+    assert_string(result, "get deployment / v1")
   end
 
   def test_get_deployment_slash_v1_slash_app
     result = sb { get.deployment/v1/app }
     assert_buffer(result, [["get", []], ["deployment", []], :slash, ["v1", []], :slash, ["app", []]])
+    assert_string(result, "get deployment / v1 / app")
   end
 
   def test_get_deployment_slash_v1_slash_app_namespace
@@ -40,26 +48,31 @@ class StringBuilderTest < Minitest::Test
         ["app", []], ["namespace", ["default"]]
       ]
     )
+    assert_string(result, "get deployment / v1 / app namespace(\"default\")")
   end
 
   def test_dash_operator_marks_dash_token
     result = sb { get.node.k8s-node }
     assert_buffer(result, [["get", []], ["node", []], ["k8s", []], :dash, ["node", []]])
+    assert_string(result, "get node k8s - node")
   end
 
   def test_blockless_chaining
     result = StringBuilder.new.get.deployment
     assert_buffer(result, [["get", []], ["deployment", []]])
+    assert_string(result, "get deployment")
   end
 
   def test_call_with_string_arg
     result = StringBuilder.new.get.("deployment/v1/apps")
     assert_buffer(result, [["get", []], ["deployment/v1/apps", []]])
+    assert_string(result, "get deployment/v1/apps")
   end
 
   def test_call_with_string_arg_then_chain
     result = StringBuilder.new.get.("deployment/v1/apps").all
     assert_buffer(result, [["get", []], ["deployment/v1/apps", []], ["all", []]])
+    assert_string(result, "get deployment/v1/apps all")
   end
 
   def test_call_with_no_args_raises
